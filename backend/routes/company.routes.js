94c3d4df3276362b104f8ff1ed2a48522b4dd2aa
Malaika Ayub <malaika22202@gmail.com>
2026-08-24 22:10:25 +0500

    Update company.routes

--- backend/routes/company.routes.js
+++ backend/routes/company.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import isAuthenticated from "../Middlewares/isAuthenticated.js";
+import { singleUpload } from "../utils/multer.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controller/company.contoller.js";
 
 const router = express.Router();
 
-router.route("/register").post(isAuthenticated,registerCompany);
-router.route("/get").get(isAuthenticated,getCompany);
-router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated,updateCompany); 
+router.route("/register").post(isAuthenticated, singleUpload, registerCompany);
+router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get/:id").get(isAuthenticated, getCompanyById);
+router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
 
 export default router;
