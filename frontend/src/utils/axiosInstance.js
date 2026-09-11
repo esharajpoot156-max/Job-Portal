5a73db4d063639d62efef3127c7bd9fb3b9ecbba
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-11 19:15:50 +0500

    deploy

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: ["http://localhost:8000/api/v1", "https://job-portal-ad90.onrender.com"],
+    baseURL: ["http://localhost:8000/api/v1", "https://job-portal-ad90.onrender.com/api"],
     withCredentials: true
 });
 
