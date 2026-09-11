77813307abe04b2df4c6a5af4164b8c444dd23a2
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-12 00:42:46 +0500

    deploy

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
-const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://job-portal-sepia-zeta.vercel.app"],
-    credentials: true
-}
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:5173", "http://localhost:5174", "https://job-portal-sepia-zeta.vercel.app"],
+        credentials: true
+    }
+});
 
-app.use(cors(corsOptions)); 
+app.use(cors(io)); 
 
 const PORT = process.env.PORT || 3000;
 
